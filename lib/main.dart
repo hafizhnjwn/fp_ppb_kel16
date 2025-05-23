86b9d28b51ee8ea7d32924b2eebbd2fc343e7778
hardytee1 <hardytee29@gmail.com>
2025-05-23 18:56:56 +0700

    initialized firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,27 @@
+import 'package:fp_pbb_kel6/screens/home.dart';
+import 'package:fp_pbb_kel6/screens/login.dart';
+import 'package:fp_pbb_kel6/screens/register.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(initialRoute: 'login', routes: {
+      'home': (context) => const HomeScreen(),
+      'login': (context) => const LoginScreen(),
+      'register': (context) => const RegisterScreen(),
+    });
   }
 }
